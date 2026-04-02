a684ee1516bc5b07da4f056211b4aa96d03bdc32
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-04-02 18:47:13 +0500

    Initial commit from Flare Codegen (API)

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Calculator from '@/components/Calculator'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-900 via-purple-900 to-gray-900">
+      <Calculator />
+    </main>
+  )
+}
